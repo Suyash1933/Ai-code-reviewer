1e9baa170fb5a3f478f50969e6698f24dae8835d
Suyash1933 <122913472+Suyash1933@users.noreply.github.com>
2026-01-11 20:42:47 +0530

    Update ai.routes.js

--- BackEnd/src/routes/ai.routes.js
+++ BackEnd/src/routes/ai.routes.js
@@ -1,10 +1,7 @@
-const express = require('express');
-const aiController = require("../controllers/ai.controller")
+const express = require("express")
+const controller = require("../controllers/ai.controller")
 
-const router = express.Router();
+const router = express.Router()
+router.post("/get-review", controller.getReview)
 
-
-router.post("/get-review", aiController.getReview)
-
-
-module.exports = router;    
+module.exports = router
